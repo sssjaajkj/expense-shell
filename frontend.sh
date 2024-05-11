b9a537ce16ebc782757fcd0bae2d31a7b06ddbda
sssjaajkj <vshiva4646@gmail.com>
2024-05-11 21:36:36 +0530

    expense-shell

--- frontend.sh
+++ frontend.sh
@@ -39,20 +39,24 @@ VALIDATE $? "Installing nginx"
 systemctl enable nginx &>>$LOGFILE
 VALIDATE $? "enable nginx"
 
-systemctl start nginx
+systemctl start nginx &>>$LOGFILE
 VALIDATE $? "start nginx"
 
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/*&>>$LOGFILE
 VALIDATE $? "Removing existing content"
 
 #Download the frontend content
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
 VALIDATE $? "Downloading front code"
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-VALIDATE $? "Extracting frontend code"
+cd /usr/share/nginx/html &>>$LOGFILE
+unzip /tmp/frontend.zip &>>$LOGFILE
+VALIDATE $? "Extracting frontend code" 
 
 vim /etc/nginx/default.d/expense.conf
 #check your repo and path
-cp
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+VALIDATE $? "Copied expense nginx"
+
+systemctl restart nginx &>>$LOGFILE
+VALIDATE $? "Restart nginx"
