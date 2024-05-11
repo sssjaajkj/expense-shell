5e6c6360f59d0ffbd1f465c42aa623fb9565d6ad
sssjaajkj <vshiva4646@gmail.com>
2024-05-11 18:13:36 +0530

    expense-shell

--- backend.sh
+++ backend.sh
@@ -51,3 +51,23 @@ if [ $USERID -ne 0 ]
     echo -e "Expense user already created ... $Y SKIPPING $N"
     fi
     
+
+    mkdir -p /app # if not creating other leave
+    VALIDATE $? "Creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+    VALIDATE $? "Downloading backend code"
+
+    cd /app 
+    unzip /tmp/backend.zip
+    VALIDATE $? "Extracted backed code"
+
+    npm install
+    VALIDATE $? "installing nodejs dependencies"
+
+    #backend.service
+    
+
+
+    
+
