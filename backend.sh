be59e52702dab081b413c1714ea8dacc1e02bf07
sssjaajkj <vshiva4646@gmail.com>
2024-05-11 20:40:31 +0530

    expense-shell

--- backend.sh
+++ backend.sh
@@ -48,6 +48,7 @@ if [ $USERID -ne 0 ]
     if [ $? -ne 0 ]
     then
     useradd expense &>>$LOGFILE
+    
     VALIDATE $? "creating expense user"
     else
     echo -e "Expense user already created ... $Y SKIPPING $N"
