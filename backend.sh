42883e0d2d1d831d8e4e8f576d7204255287391e
sssjaajkj <vshiva4646@gmail.com>
2024-05-11 22:06:22 +0530

    expense-shell

--- backend.sh
+++ backend.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
  SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
@@ -9,10 +8,11 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-echo "Please enter DB password"
-read  mysql_root_password
 
-VALIDATE () {
+echo "Please enter DB password"
+read -s mysql_root_password
+ 
+VALIDATE() {
         if [ $1 -ne 0 ]
             then
              echo -e "$2 ... $R Failure... $N"
@@ -32,77 +32,66 @@ if [ $USERID -ne 0 ]
             echo "you are super user."
     fi
 
-    dnf module disable nodejs -y &>>$LOGFILE
-    VALIDATE $? "Disabling default nodejs"
+    
+#     # id expense &>>$LOGFILE
+#     # if [ $? -ne 0 ]
+#     # then
+#     #     useradd expense &>>$LOGFILE
+#     # else
+#     #     echo -e "Expense user already created...$Y SKIPPING $N"
+#     # fi
 
-    dnf module enable nodejs:20 -y &>>$LOGFILE
-    VALIDATE $? "Enable nodejs:20 Version"
-
-    dnf install nodejs -y &>>$LOGFILE
-    VALIDATE $? "installing  nodejs"
-
-    useradd expense
-    VALIDATE $? "Creating USERADDING expense"
-
-    # id expense &>>$LOGFILE
-    # if [ $? -ne 0 ]
-    # then
-    #     useradd expense &>>$LOGFILE
-    # else
-    #     echo -e "Expense user already created...$Y SKIPPING $N"
-    # fi
-
-    # id expense &>>$LOGFILE
-    # if [ $? -ne 0 ]
-    # then
-    #     useradd expense &>>$LOGFILE
-    #    VALIDATE $? "creating expense user"
-    # else
-    #     echo -e "Expense user already created ... $Y SKIPPING $N"
-    # fi
+#     # id expense &>>$LOGFILE
+#     # if [ $? -ne 0 ]
+#     # then
+#     #     useradd expense &>>$LOGFILE
+#     #    VALIDATE $? "creating expense user"
+#     # else
+#     #     echo -e "Expense user already created ... $Y SKIPPING $N"
+#     # fi
     
 
-    mkdir -p /app &>>$LOGFILE  
-    VALIDATE $? "Creating app directory"
+#     mkdir -p /app &>>$LOGFILE  
+#     VALIDATE $? "Creating app directory"
 
-    curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE 
-    VALIDATE $? "Downloading backend code"
+#     curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE 
+#     VALIDATE $? "Downloading backend code"
     
-    echo "Downloaded"
-    cd /app
-    rm -rf /app/*
-    unzip /tmp/backend &>>$LOGFILE
-    VALIDATE $? "Extracted backed code"
+#     echo "Downloaded"
+#     cd /app
+#     rm -rf /app/*
+#     unzip /tmp/backend &>>$LOGFILE
+#     VALIDATE $? "Extracted backed code"
 
-    npm install -y &>>$LOGFILE
-    VALIDATE $? "installing nodejs dependencies"
+#     npm install -y &>>$LOGFILE
+#     VALIDATE $? "installing nodejs dependencies"
 
-    #backend.service
-    cp /home/ec2-user/expense-shell/backend.service  /etc/systemd/system/backend.service
+#     #backend.service
+#     cp /home/ec2-user/expense-shell/backend.service  /etc/systemd/system/backend.service
 
-    VALIDATE $? "Copied backed service"
+#     VALIDATE $? "Copied backed service"
 
-    systemctl daemon-reload &>>$LOGFILE
-    VALIDATE $? "daemon-reload"
+#     systemctl daemon-reload &>>$LOGFILE
+#     VALIDATE $? "daemon-reload"
 
-   #Start the service.
-    systemctl start backend  &>>$LOGFILE
-    VALIDATE $? "start backend"
+#    #Start the service.
+#     systemctl start backend  &>>$LOGFILE
+#     VALIDATE $? "start backend"
     
-    systemctl enable backend &>>$LOGFILE
-    VALIDATE $? "enable backend"
+#     systemctl enable backend &>>$LOGFILE
+#     VALIDATE $? "enable backend"
 
     
     
-    dnf install mysql -y &>>$LOGFILE
-    VALIDATE $? "Installing Mysql client"
+#     dnf install mysql -y &>>$LOGFILE
+#     VALIDATE $? "Installing Mysql client"
     
 
-    mysql -h db.aws79s.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
-    VALIDATE $? "Schema loading "
+#     mysql -h db.aws79s.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+#     VALIDATE $? "Schema loading "
 
-    systemctl restart backend &>>$LOGFILE
-    VALIDATE $? "Restart backend "
+#     systemctl restart backend &>>$LOGFILE
+#     VALIDATE $? "Restart backend "
 
 
 
