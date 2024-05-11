8cfaa598567689988c802e12c2d326ed106a47d5
sssjaajkj <vshiva4646@gmail.com>
2024-05-11 16:47:11 +0530

    expense-shell

--- mysql.sh
+++ mysql.sh
@@ -9,16 +9,14 @@ R="\e[31m"
 G="\e[32m"
 y="\e[33m"
 N="\e[0m"
-
-
 VALIDATE ()
         {
         if [ $1 -ne 0]
             then
              echo -e "$2 ... $R Failure... $N"
-                 exit  1
+                exit  1
           else
-          echo -e "$2 ...  $G Pass... $N"
+          echo -e "$2 ...  $G Success... $N"
             fi   
             }
 
