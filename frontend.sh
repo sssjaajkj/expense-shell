e9915bc9b3000050427766ec4421eb2d179fbd05
sssjaajkj <vshiva4646@gmail.com>
2024-05-11 21:27:18 +0530

    expense-shell

---
+++ frontend.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+ LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "Please enter DB password"
+read -s mysql_root_password
+
+VALIDATE () {
+        if [ $1 -ne 0 ]
+            then
+             echo -e "$2 ... $R Failure... $N"
+                exit  1
+          else
+          echo -e "$2 ...  $G Success... $N"
+            fi   
+            }
+
+
+if [ $USERID -ne 0 ] 
+ then 
+    echo "plz run this script with root access."
+    exit 1 # manulaly exit if error comes 
+
+    else 
+            echo "you are super user."
+    fi
+
+dnf instll nginx -y &>>$LOGFILE
+VALIDATE $? "Installing ngimx"
+
+systemctl enable nginx &>>$LOGFILE
+VALIDATE $? "enable nginx"
+
+systemctl start nginx
+VALIDATE $? "start nginx"
+
+rm -rf /usr/share/nginx/html/*
+VALIDATE $? "Removing existing content"
+
+#Download the frontend content
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+VALIDATE $? "Downloading front code"
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+VALIDATE $? "Extracting frontend code"
+
+vim /etc/nginx/default.d/expense.conf
